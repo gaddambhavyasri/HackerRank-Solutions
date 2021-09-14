2e237073f722b66389da369f83f303113840bfc2
gaddambhavyasri <89065809+gaddambhavyasri@users.noreply.github.com>
2021-09-14 16:29:16 +0530

    Update Python If-Else.py

--- Python/Python If-Else.py
+++ Python/Python If-Else.py
@@ -1,7 +1,7 @@
 //this is a short liner and easy one.
 
-n = int(input())
-if(n%2==1) or n in range (5,21):
+n = int(input("enter the integer:"))
+if(n%2==1) or n in range (0,n-1):
     print("Weird")
 else:
     print("Not Weird")
